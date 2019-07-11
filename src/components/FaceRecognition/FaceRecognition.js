e93784f1e187e6ee3191deb63933a6435497821d
jrodowick <jacobrodowick@gmail.com>
2019-07-11 15:30:21 -0700

    Adds component to hold image

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+  return (
+    <div className='center mt3'>
+      <img alt='' src='https://cdn.images.express.co.uk/img/dynamic/11/590x/skincare-994444.jpg?r=1532587813853' width='30%' height='auto'/>
+    </div>
+  )
+}
+
+export default FaceRecognition;
